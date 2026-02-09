3252f3e3106502886bc6ae352d6c71184220c216
Arthur <arthurdev.contatoo@gmail.com>
2026-02-09 16:35:32 -0300

    correcoes no serializers e criacao de usuario teste para testes

--- agendamentos/admin.py
+++ agendamentos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from agendamentos.models import Agendamento
 
-# Register your models here.
+
+class AgendamentoAdmin(admin.ModelAdmin):
+    list_display = ('hora_inicio', 'hora_fim')
+
+admin.site.register(Agendamento, AgendamentoAdmin)
--- agendamentos/serializers.py
+++ agendamentos/serializers.py
@@ -1,48 +1,75 @@
 from rest_framework import serializers
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 from datetime import datetime, timedelta
 from servicos.models import Servico
 from agendamentos.models import Agendamento
 
 
 class AgendamentoSerializer(serializers.ModelSerializer):
-    data_hoje = datetime.now().date()
+    '''duracao = serializers.SerializerMethodField(read_only=True)
+    preco_total = serializers.SerializerMethodField(read_only=True)
+    eh_futuro = serializers.SerializerMethodField(read_only=True)
+    eh_hoje = serializers.SerializerMethodField(read_only=True)'''
 
     class Meta:
         model = Agendamento
         fields = '__all__'
+        read_only_fields = ['hora_fim', 'usuario', 'status']
     
-    def calcular_hora_fim(self):
-        servico = self.validated_data.get('servico')
-        hora_inicio = self.validated_data.get('hora_inicio')
-        data = self.validated_data.get('data')
+    def validate(self, attrs):
+        servico = attrs.get('servico')
+        cliente = attrs.get('cliente')
+        data_agendamento = attrs.get('data')
+        hora_inicio = attrs.get('hora_inicio')
+        usuario = self.context['request'].user
 
-        # Montando data e hora com a data e hora de início para calcular com timedelta
-        data_hora = datetime.combine(data, hora_inicio)
-        return (data_hora + timedelta(minutes=servico.duracao_minutos)).time()
+        user_test = User.objects.first()
+
+        if not all([servico, cliente, data_agendamento, hora_inicio]):
+            return attrs
+
+        if not servico.usuario.id == cliente.usuario.id:
+            raise serializers.ValidationError('O serviço e o cliente devem pertencer ao mesmo usuário.')
+        
+        hora_fim = (datetime.combine(data_agendamento, hora_inicio) + timedelta(minutes=servico.duracao_minutos)).time()
+        conflitos = user_test.agendamentos.filter(
+            status='agendado', 
+            data=data_agendamento,
+            hora_inicio__lte=hora_fim,
+            hora_fim__gte=hora_inicio
+        )
+        
+        if self.instance:
+            conflitos = conflitos.exclude(pk=self.instance.pk)
+        
+        if conflitos:
+            raise serializers.ValidationError({'hora_inicio': 'A empresa já possui um agendamento nesse horário.'})
+        
+        return attrs
     
     def validate_data(self, value):
-        if value < self.data_hoje:
+        if value < datetime.now().date():
             raise serializers.ValidationError('A data informada já passou.')
         
         return value
-    
-    def validate_hora_inicio(self, value):
-        data_agendamento = self.initial_data.get('data')
-        data_hoje = datetime.strftime(self.data_hoje, "%Y-%m-%d")
-
-        if data_agendamento == data_hoje:
-            if value < datetime.now().time():
-                raise serializers.ValidationError('O horário de início já passou.')
-            
-        return value
 
     def create(self, validated_data):
-        validated_data['hora_fim'] = self.calcular_hora_fim()
+        servico = validated_data['servico']
+        hora_inicio = validated_data['hora_inicio']
+        data = validated_data['data']
+
+        user_test = User.objects.first()
+        
+        # A hora final é a hora inicial + tempo de duração do serviço
+        validated_data['hora_fim'] = (datetime.combine(data, hora_inicio) + timedelta(minutes=servico.duracao_minutos)).time()
+        validated_data['usuario'] = user_test
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
-        if instance.pk:
-            instance.hora_fim = self.calcular_hora_fim()
+        servico = validated_data.get('servico', instance.servico)
+        data = validated_data.get('data', instance.data)
+        hora_inicio = validated_data.get('hora_inicio', instance.hora_inicio)
 
+        instance.hora_fim = (datetime.combine(data, hora_inicio) + timedelta(minutes=servico.duracao_minutos)).time()
         return super().update(instance, validated_data)
